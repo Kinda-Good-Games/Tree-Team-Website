bbef4b6c82b9d3cb17fb035033f1bbdfb5a72b65
Your Name <you@example.com>
2022-11-25 11:13:33 +0100

    donation website and thank you website

--- donation.js
+++ donation.js
@@ -1,12 +1,36 @@
 const btn = document.querySelector("button");
+const eins = document.getElementById("1");
+const zwei = document.getElementById("2");
+const drei = document.getElementById("3");
+const vier = document.getElementById("4");
+const fünf = document.getElementById("5");
+const sechs = document.getElementById("6");
 
-btn.addEventListener("click", floattext, true);
-btn.addEventListener("click", rmvtext, true)
 
-function floattext() {
-    btn.style.border = "3px solid white"
+function chngstt1() {
+    eins.classList.toggle("clickstt");
 }
 
-function rmvtext() {
-    btn.style.border = "none"
+function chngstt2() {
+    zwei.classList.toggle("clickstt");
+}
+
+function chngstt3() {
+    drei.classList.toggle("clickstt");
+}
+
+function chngstt4() {
+    vier.classList.toggle("clickstt");
+}
+
+function chngstt5() {
+    fünf.classList.toggle("clickstt");
+}
+
+function chngstt6() {
+    sechs.classList.toggle("clickstt");
+}
+
+function donate() {
+    window.open("thankyou.html")
 }
