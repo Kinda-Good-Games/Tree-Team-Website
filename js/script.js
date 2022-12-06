25ef1bbcdf1c828695ea0c0f78b96e6db322ead4
Your Name <you@example.com>
2022-12-06 13:07:54 +0100

    game for mobile removed

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const mobile_Game = document.querySelector(".mobile-game");
+const window_size = window.matchMedia("(max-width: 600px)");
+
+function gamemobile(window_size) {
+    if (window_size.matches) {
+      mobile_Game.style.display = "grid";
+    } else {
+        mobile_Game.style.display = "none";
+    }
+  }
+
+gamemobile(window_size);
+window_size.addListener(gamemobile);
